f918ef9887231997ea7e762bb1b73aa9e1fe2b14
JalenMuller <mjalen251@gmail.com>
2023-05-20 01:15:58 +0200

    Load react from blade file (Deployment)

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Groovify</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+    <link rel="stylesheet" href="{{ URL::asset('app/index.css') }}">
+    <script type="module" crossorigin src="{{ URL::asset('app/app.js') }}"></script>
+    <link rel="stylesheet" href="{{ URL::asset('app/app.css') }}">
+</head>
+
+<body>
+    <div id="root"></div>
+
+</body>
+
+</html>
